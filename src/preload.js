a5c51081547ef75f79568a1392971fd70a127d54
abuasadUK <idons11@gmail.com>
2026-06-21 03:26:43 +0100

    Delete src directory

--- src/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("sortZoneDesktop", {
-  listPrinters: () => ipcRenderer.invoke("printers:list"),
-  silentPrint: (payload) => ipcRenderer.invoke("print:silent", payload),
-  rawZplPrint: (payload) => ipcRenderer.invoke("print:zpl", payload),
-  openKeyboard: () => ipcRenderer.invoke("keyboard:open")
-});
